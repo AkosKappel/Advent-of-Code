589941dad628ed5b730de372b106830d6864cb0c
Ákos Kappel <kappelakos@gmail.com>
2022-12-04 08:18:46 +0100

    day 4

--- 2022/src/day04.ts
+++ 2022/src/day04.ts
@@ -1,15 +1,18 @@
-const parse = (s: string) => s.trim();
+const parse = (s: string) => s.trim()
+  .split('\n')
+  .map(line => line.split(',')
+    .map(range => range.split('-').map(Number)));
 
-export const part1 = (s: string) => {
-  parse(s);
-  return 0;
-};
+export const part1 = (s: string) => parse(s)
+  .filter(([[s1, e1], [s2, e2]]) =>
+    s1 <= s2 && e2 <= e1 || s2 <= s1 && e1 <= e2)
+  .length;
 
 exports.first = part1;
 
-export const part2 = (s: string) => {
-  parse(s);
-  return 0;
-};
+export const part2 = (s: string) => parse(s)
+  .filter(([[s1, e1], [s2, e2]]) =>
+    Math.max(s1, s2) <= Math.min(e1, e2))
+  .length;
 
 exports.second = part2;
--- 2022/tests/day05.test.ts
+++ 2022/tests/day05.test.ts
@@ -1,19 +1,14 @@
 import { existsSync, readFileSync } from 'fs';
-import { part1, part2 } from '../src/day04';
+import { part1, part2 } from '../src/day05';
 
-const day: string = '04';
+const day: string = '05';
 const exampleInput = `
-2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8
+
 `;
-const exampleAnswer1 = 2;
-const exampleAnswer2 = 4;
-const puzzleAnswer1 = 483;
-const puzzleAnswer2 = 874;
+const exampleAnswer1 = 0;
+const exampleAnswer2 = 0;
+const puzzleAnswer1 = 0;
+const puzzleAnswer2 = 0;
 
 const inputFile: string = `input/day${day}.in`;
 const puzzleInput: string = existsSync(inputFile) ? readFileSync(inputFile, 'utf8') : '0';
