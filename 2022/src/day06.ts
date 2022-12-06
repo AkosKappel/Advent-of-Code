2ba451d03d336236ff86f6cd5cd8810efc169994
Ákos Kappel <kappelakos@gmail.com>
2022-12-06 14:35:20 +0100

    day 6 functional solution

--- 2022/src/day06.ts
+++ 2022/src/day06.ts
@@ -1,23 +1,15 @@
-const allUnique = (s: string) => s.length === new Set(s).size;
+const allUniqueChars = (s: string) => s.length === new Set(s).size;
 
-const getMessageStart = (s: string, markerLength: number): number => {
-  const signal = s.trim();
-  let marker = signal.substring(0, markerLength);
+const findMessageStart = (message: string, markerLength: number) => message
+  .split('')
+  .map((char: string, i: number) => message.substring(i, i + markerLength))
+  .filter((chunk: string) => chunk.length === markerLength)
+  .findIndex((chunk: string) => allUniqueChars(chunk)) + markerLength;
 
-  let i = markerLength;
-  while (!allUnique(marker)) {
-    i++;
-    if (i > signal.length) throw new Error(`No message start sequence found in signal ${signal}`);
-    marker = signal.substring(i - markerLength, i);
-  }
-
-  return i;
-};
-
-export const part1 = (s: string) => getMessageStart(s, 4);
+export const part1 = (s: string) => findMessageStart(s.trim(), 4);
 
 exports.first = part1;
 
-export const part2 = (s: string) => getMessageStart(s, 14);
+export const part2 = (s: string) => findMessageStart(s.trim(), 14);
 
 exports.second = part2;
