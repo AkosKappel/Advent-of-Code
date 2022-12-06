39776dc8d732bc8fafe812f461ad36677fdf131d
Ákos Kappel <kappelakos@gmail.com>
2022-12-06 10:14:52 +0100

    refactor day 6

--- 2022/src/day06.ts
+++ 2022/src/day06.ts
@@ -1,35 +1,23 @@
-const parse = (s: string) => s.trim();
-
 const allUnique = (s: string) => s.length === new Set(s).size;
 
-export const part1 = (s: string) => {
-  const signal = parse(s);
-  const markerLength = 4;
+const getMessageStart = (s: string, markerLength: number): number => {
+  const signal = s.trim();
   let marker = signal.substring(0, markerLength);
 
   let i = markerLength;
   while (!allUnique(marker)) {
     i++;
+    if (i > signal.length) throw new Error(`No message start sequence found in signal ${signal}`);
     marker = signal.substring(i - markerLength, i);
   }
 
   return i;
 };
+
+export const part1 = (s: string) => getMessageStart(s, 4);
 
 exports.first = part1;
 
-export const part2 = (s: string) => {
-  const signal = parse(s);
-  const markerLength = 14;
-  let marker = signal.substring(0, markerLength);
-
-  let i = markerLength;
-  while (!allUnique(marker)) {
-    i++;
-    marker = signal.substring(i - markerLength, i);
-  }
-
-  return i;
-};
+export const part2 = (s: string) => getMessageStart(s, 14);
 
 exports.second = part2;
