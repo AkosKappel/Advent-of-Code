9178e09edeb4950f6e3138f0b2620d40b12f9670
Ákos Kappel <kappelakos@gmail.com>
2022-12-07 16:07:58 +0100

    refactor day 7

--- 2022/src/day07.ts
+++ 2022/src/day07.ts
@@ -1,129 +1,49 @@
-interface File {
-  type: 'file';
-  size: number;
-}
+const MAX_DIR_SIZE: number = 100_000;
 
-interface Directory {
-  type: 'dir';
-  children: Record<string, Entity>;
-}
+const TOTAL_SPACE: number = 70_000_000;
+const NEEDED_SPACE: number = 30_000_000;
 
-type Entity = File | Directory;
+const ROOT: string = '/';
 
-interface CommandProps {
-  args: string[];
-  output: string[];
-}
+const getFoldersSize = (s: string) => {
+  const path: string[] = [];
+  const folders: { [key: string]: number } = {};
 
-function buildFilesystem(data: string): Directory {
-  const root: Directory = {
-    type: 'dir',
-    children: {},
-  };
-  let path: string[] = [];
-
-  function getCwd(entity: Directory, path: string[]): Directory {
-    if (!path.length) return entity;
-    const [dir, ...rest] = path;
-    return getCwd(entity.children[dir] as Directory, rest);
-  }
-
-  function cd({ args: [destination] }: CommandProps) {
-    if (destination === '/') path = [];
-    else if (destination === '..') path.pop();
-    else path.push(destination);
-  }
-
-  function ls({ output }: CommandProps) {
-    const cwd = getCwd(root, path);
-
-    for (const line of output) {
-      if (line.startsWith('dir')) {
-        const dir = line.slice(4);
-        cwd.children[dir] = {
-          type: 'dir',
-          children: {},
-        };
-      } else {
-        const [size, name] = line.split(' ');
-        cwd.children[name] = {
-          type: 'file',
-          size: parseInt(size),
-        };
+  s.trim().split('\n').forEach(line => {
+    if (line.startsWith('$ cd ..')) { // go up a folder
+      path.pop();
+    } else if (line.startsWith('$ cd ')) { // go down a folder
+      path.push(line.trim().slice(5));
+    } else if (line.startsWith('$ ls')) { // list files and folders
+      // pass
+    } else if (line.startsWith('dir ')) { // folder
+      // pass
+    } else { // file
+      const [size, name] = line.split(' ');
+      for (let i = 0; i < path.length; i++) {
+        const folder = path.slice(0, i + 1).join('/');
+        folders[folder] = (folders[folder] || 0) + parseInt(size);
       }
     }
-  }
+  });
 
-  for (const entry of data.split(/^\$ /gm)) {
-    if (!entry) continue;
-
-    const [input, ...output] = entry.split('\n').map(line => line.trim()).filter(Boolean);
-    const [command, ...args] = input.split(' ');
-
-    switch (command) {
-      case 'cd':
-        cd({ args, output });
-        break;
-      case 'ls':
-        ls({ args, output });
-        break;
-    }
-  }
-
-  return root;
-}
-
-function getSize(entity: Entity): number {
-  return entity.type === 'dir' ?
-    Object.values(entity.children)
-      .reduce((size: number, child: Entity) => size + getSize(child), 0) :
-    entity.size;
-}
-
-export const part1 = (s: string) => {
-  const MAX_DIR_SIZE: number = 100_000;
-
-  const root = buildFilesystem(s.trim());
-  const foundSizes: number[] = [];
-
-  const stack: Directory[] = [root];
-  while (stack.length) {
-    const entity = stack.pop()!;
-
-    let size = 0;
-    for (const child of Object.values(entity.children)) {
-      size += getSize(child);
-      if (child.type === 'dir') stack.push(child);
-    }
-    if (size <= MAX_DIR_SIZE) foundSizes.push(size);
-  }
-
-  return foundSizes.reduce((a: number, b: number) => a + b, 0);
+  return folders;
 };
+
+export const part1 = (s: string) => Object
+  .values(getFoldersSize(s))
+  .filter(size => size <= MAX_DIR_SIZE)
+  .reduce((a: number, b: number) => a + b, 0);
 
 exports.first = part1;
 
 export const part2 = (s: string) => {
-  const TOTAL_SPACE: number = 70_000_000;
-  const NEEDED_SPACE: number = 30_000_000;
-
-  const root = buildFilesystem(s.trim());
-  const used = getSize(root);
-  const needToDelete = used - TOTAL_SPACE + NEEDED_SPACE;
-
-  function recur(entity: Entity): number[] {
-    if (entity.type === 'file') return [];
-
-    const size = getSize(entity);
-    const deletables = size > needToDelete ? [size] : [];
-
-    for (const child of Object.values(entity.children)) {
-      deletables.push(...recur(child));
-    }
-    return deletables;
-  }
-
-  return Math.min(...recur(root));
+  const folders = getFoldersSize(s);
+  const FREE_SPACE: number = TOTAL_SPACE - folders[ROOT];
+  return Object
+    .values(folders)
+    .filter(size => size + FREE_SPACE >= NEEDED_SPACE)
+    .reduce((a: number, b: number) => a < b ? a : b, Infinity);
 };
 
 exports.second = part2;
