333cd8570b5d06dda92e5d1320848584b70bd71e
Ákos Kappel <kappelakos@gmail.com>
2022-12-08 08:36:43 +0100

    refactor day 8

--- 2022/src/day08.ts
+++ 2022/src/day08.ts
@@ -1,61 +1,45 @@
-import { input, answer1, answer2 } from '../examples/day08.input';
-
 const parse = (s: string) => s.trim()
   .split('\n',
   ).map(row => row.split(''));
 
-export const part1 = (s: string) => {
+const DIR = {
+  UP: [0, -1],
+  DOWN: [0, 1],
+  LEFT: [-1, 0],
+  RIGHT: [1, 0],
+};
+
+const isTallest = (dir: number[], i: number, j: number, forest: string[][]): boolean => {
+  const tree = forest[i][j];
+  const [dx, dy] = dir;
+
+  let [x, y] = [j + dx, i + dy];
+  let tallest = true;
+
+  while (forest[y] && forest[y][x]) {
+    const nextTree = forest[y][x];
+
+    if (nextTree >= tree) {
+      tallest = false;
+      break;
+    }
+
+    [x, y] = [x + dx, y + dy];
+  }
+
+  return tallest;
+};
+
+export const part1 = (s: string): number => {
   const trees = parse(s);
   let n = 0;
 
   for (let i = 0; i < trees.length; i++) {
     for (let j = 0; j < trees[i].length; j++) {
-      const tree = trees[i][j];
-      // look up
-      let tallest = true;
-      for (let k = i - 1; k >= 0; k--) {
-        if (trees[k][j] >= tree) {
-          tallest = false;
-          break;
-        }
-      }
-      if (tallest) {
-        n++;
-        continue;
-      }
-      // look down
-      tallest = true;
-      for (let k = i + 1; k < trees.length; k++) {
-        if (trees[k][j] >= tree) {
-          tallest = false;
-          break;
-        }
-      }
-      if (tallest) {
-        n++;
-        continue;
-      }
-      // look left
-      tallest = true;
-      for (let k = j - 1; k >= 0; k--) {
-        if (trees[i][k] >= tree) {
-          tallest = false;
-          break;
-        }
-      }
-      if (tallest) {
-        n++;
-        continue;
-      }
-      // look right
-      tallest = true;
-      for (let k = j + 1; k < trees[i].length; k++) {
-        if (trees[i][k] >= tree) {
-          tallest = false;
-          break;
-        }
-      }
-      if (tallest) {
+      if (isTallest(DIR.UP, i, j, trees) ||
+        isTallest(DIR.DOWN, i, j, trees) ||
+        isTallest(DIR.LEFT, i, j, trees) ||
+        isTallest(DIR.RIGHT, i, j, trees)) {
         n++;
       }
     }
@@ -63,85 +47,70 @@ export const part1 = (s: string) => {
 
   return n;
 };
-console.log(part1(input));
-console.log(answer1);
 
 exports.first = part1;
 
-export const part2 = (s: string) => {
+const getDirScore = (dir: number[], i: number, j: number, forest: string[][]): number => {
+  const tree = forest[i][j];
+  const [dx, dy] = dir;
+
+  let [x, y] = [j + dx, i + dy];
+  let tallest = true;
+  let scenicScore = 0;
+
+  while (forest[y] && forest[y][x]) {
+    const nextTree = forest[y][x];
+
+    if (nextTree >= tree) {
+      tallest = false;
+      if (dir === DIR.UP) {
+        scenicScore = i - y;
+      } else if (dir === DIR.DOWN) {
+        scenicScore = y - i;
+      } else if (dir === DIR.LEFT) {
+        scenicScore = j - x;
+      } else if (dir === DIR.RIGHT) {
+        scenicScore = x - j;
+      }
+      break;
+    }
+
+    [x, y] = [x + dx, y + dy];
+  }
+
+  if (tallest) {
+    if (dir === DIR.UP) {
+      scenicScore = i;
+    } else if (dir === DIR.DOWN) {
+      scenicScore = forest.length - i - 1;
+    } else if (dir === DIR.LEFT) {
+      scenicScore = j;
+    } else if (dir === DIR.RIGHT) {
+      scenicScore = forest[i].length - j - 1;
+    }
+  }
+
+  return scenicScore;
+};
+
+export const part2 = (s: string): number => {
   const trees = parse(s);
   let bestScenicScore = 0;
 
   for (let i = 0; i < trees.length; i++) {
     for (let j = 0; j < trees[i].length; j++) {
 
-      const tree = trees[i][j];
-      let scenicScore = 1;
+      const scoreUp = getDirScore(DIR.UP, i, j, trees);
+      const scoreDown = getDirScore(DIR.DOWN, i, j, trees);
+      const scoreLeft = getDirScore(DIR.LEFT, i, j, trees);
+      const scoreRight = getDirScore(DIR.RIGHT, i, j, trees);
 
-      // look up
-      let tallest = true;
-      for (let k = i - 1; k >= 0; k--) {
-        if (trees[k][j] >= tree) {
-          tallest = false;
-          // console.log('up', i, j, k, trees[k][j], tree, 'i - k', i - k);
-          scenicScore *= (i - k);
-          break;
-        }
-      }
-      if (tallest) {
-        // console.log('up', i, j, 'i', i);
-        scenicScore *= (i);
-      }
-      // look down
-      tallest = true;
-      for (let k = i + 1; k < trees.length; k++) {
-        if (trees[k][j] >= tree) {
-          tallest = false;
-          // console.log('down', i, j, k, trees[k][j], tree, 'k - i', k - i);
-          scenicScore *= (k - i);
-          break;
-        }
-      }
-      if (tallest) {
-        // console.log('down', i, j, 'trees.length - i', trees.length - i - 1);
-        scenicScore *= (trees.length - i - 1);
-      }
-      // look left
-      tallest = true;
-      for (let k = j - 1; k >= 0; k--) {
-        if (trees[i][k] >= tree) {
-          tallest = false;
-          // console.log('left', i, j, k, trees[i][k], tree, 'j - k', j - k);
-          scenicScore *= (j - k);
-          break;
-        }
-      }
-      if (tallest) {
-        // console.log('left', i, j, 'j', j);
-        scenicScore *= (j);
-      }
-      // look right
-      tallest = true;
-      for (let k = j + 1; k < trees[i].length; k++) {
-        if (trees[i][k] >= tree) {
-          tallest = false;
-          // console.log('right', i, j, k, trees[i][k], tree, 'k - j', k - j);
-          scenicScore *= (k - j);
-          break;
-        }
-      }
-      if (tallest) {
-        // console.log('right', i, j, 'trees[i].length - j - 1', trees[i].length - j - 1);
-        scenicScore *= (trees[i].length - j - 1);
-      }
-
+      const scenicScore = scoreUp * scoreDown * scoreLeft * scoreRight;
       bestScenicScore = Math.max(bestScenicScore, scenicScore);
     }
   }
 
   return bestScenicScore;
 };
-console.log(part2(input));
-console.log(answer2);
 
 exports.second = part2;
