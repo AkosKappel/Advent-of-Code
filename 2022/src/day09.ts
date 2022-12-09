b74998d5f5c1ad99491f7dec4a9dc80db423973a
Ákos Kappel <kappelakos@gmail.com>
2022-12-09 17:22:06 +0100

    refactor day 9

--- 2022/src/day09.ts
+++ 2022/src/day09.ts
@@ -1,4 +1,4 @@
-const DIRECTION = {
+const DIRECTION: { [dir: string]: number[] } = {
   U: [0, -1],
   D: [0, 1],
   L: [-1, 0],
@@ -8,17 +8,18 @@ const DIRECTION = {
 const parse = (s: string): [number[], number][] => s.trim()
   .split('\n')
   .map(row => row.split(' '))
-  .map(([dir, value]) => [
-    Object.values(DIRECTION)[Object.keys(DIRECTION).indexOf(dir as keyof typeof DIRECTION)],
-    parseInt(value, 10)],
-  );
+  .map(([dir, value]) => [DIRECTION[dir], parseInt(value, 10)]);
+
+const tooFar = (x: number): boolean => x < -1 || 1 < x; // Math.abs(x) > 1
+
+const round = (n: number): number => n > 0 ? Math.ceil(n) : Math.floor(n);
 
 const moveRope = (instructions: [number[], number][], ropeLength: number): number => {
   const start = [0, 0];
   const rope = new Array(ropeLength).fill(0).map(() => [...start]);
   const [head, tail] = [rope[0], rope[ropeLength - 1]];
 
-  const visited = new Set([tail.join(',')]);
+  const visited = new Set();
 
   for (const [dir, steps] of instructions) {
     for (let step = 0; step < steps; step++) {
@@ -29,13 +30,17 @@ const moveRope = (instructions: [number[], number][], ropeLength: number): numbe
         if (current === head) { // move only the head of the rope
           head[0] += dir[0];
           head[1] += dir[1];
+          // head.forEach((_: number, j: number) => head[j] += dir[j]);
         }
 
         const dist = [current[0] - next[0], current[1] - next[1]];
-        if (dist.some(x => -1 > x || x > 1)) {
+        // const dist = current.map((_: number, j: number) => current[j] - next[j]);
+
+        if (dist.some(tooFar)) {
           const [dx, dy] = dist.map(x => x / 2);
-          next[0] += dx > 0 ? Math.ceil(dx) : Math.floor(dx);
-          next[1] += dy > 0 ? Math.ceil(dy) : Math.floor(dy);
+          next[0] += round(dx);
+          next[1] += round(dy);
+          // next.forEach((_: number, j: number) => next[j] += round(dist[j] / 2));
         }
       }
 
--- 2022/src/day10.ts
+++ 2022/src/day10.ts
@@ -1,9 +1,13 @@
+import * as day from '../examples/day10.input';
+
 const parse = (s: string) => s.trim();
 
 export const part1 = (s: string): number => {
   parse(s);
   return 0;
 };
+console.log(part1(day.input));
+console.log(day.answer1);
 
 exports.first = part1;
 
@@ -11,5 +15,7 @@ export const part2 = (s: string): number => {
   parse(s);
   return 0;
 };
+console.log(part2(day.input));
+console.log(day.answer2);
 
 exports.second = part2;
