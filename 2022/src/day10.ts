144985fe85b7f9e12422764b6bcd60a639cabaaf
Ákos Kappel <kappelakos@gmail.com>
2022-12-10 08:11:03 +0100

    day 10 part 2

--- 2022/src/day10.ts
+++ 2022/src/day10.ts
@@ -1,5 +1,3 @@
-import * as day from '../examples/day10.input';
-
 const parse = (s: string) => s.trim()
   .split('\n')
   .map(x => x.trim().split(' '))
@@ -10,21 +8,20 @@ export const part1 = (s: string): number => {
   let ticks = 0;
   let register = 1;
   let signalStrength = 0;
-  let signalStrengthAcc = 0;
+  let acc = 0;
 
   for (let i = 0; i < program.length; i++) {
     const { op, arg } = program[i];
     if (op === 'addx') {
       ticks += 2;
-      // console.log(register);
 
       if (ticks % 40 === 20) {
         signalStrength = register * ticks;
-        signalStrengthAcc += signalStrength;
+        acc += signalStrength;
         // console.log(`tick ${ticks} register ${register}, signal strength ${signalStrength}`);
       } else if (ticks % 40 === 21) {
         signalStrength = register * (ticks - 1);
-        signalStrengthAcc += signalStrength;
+        acc += signalStrength;
         // console.log(`tick ${ticks - 1} register ${register}, signal strength ${signalStrength}`);
       }
 
@@ -34,17 +31,15 @@ export const part1 = (s: string): number => {
 
       if (ticks % 40 === 20) {
         signalStrength = register * ticks;
-        signalStrengthAcc += signalStrength;
+        acc += signalStrength;
         // console.log(`tick ${ticks} register ${register}, signal strength ${register * ticks}`);
       }
     }
 
   }
 
-  return signalStrengthAcc;
+  return acc;
 };
-console.log(part1(day.input));
-console.log(day.answer1);
 
 exports.first = part1;
 
@@ -56,34 +51,33 @@ const showScreen = (screen: string[][]): string =>
 
 export const part2 = (s: string): string => {
   const program = parse(s);
-  let ticks = 0;
-  let register = 0;
-
-  const spriteLength = 3;
 
   const [width, height] = [40, 6];
   const screen = createScreen(width, height);
 
-  for (let i = 0; i < program.length; i++) {
-    const { op, arg } = program[i];
+  let cycle = 0;
+  const tick = () => {
+    cycle++;
+    const cursor = (cycle - 1) % 40;
+    const [row, col] = [~~((cycle - 1) / 40), cursor];
+    screen[row][col] = cursor >= x - 1 && cursor <= x + 1 ? '#' : '.';
+  };
 
-    if (register >= ticks && register <= ticks + spriteLength) {
-      const [x, y] = [ticks % width, Math.floor(ticks / width)];
-      screen[y][x] = '#';
+  let x = 1;
+  for (const { op, arg } of program) {
+    switch (op) {
+      case 'noop':
+        tick();
+        break;
+      case 'addx':
+        tick();
+        tick();
+        x += arg;
+        break;
     }
-
-    if (op === 'addx') {
-      ticks += 2;
-      register += arg;
-    } else if (op === 'noop') {
-      ticks++;
-    }
-
   }
 
   return showScreen(screen);
 };
-console.log(part2(day.input));
-console.log(day.answer2);
 
 exports.second = part2;
