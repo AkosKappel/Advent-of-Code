8a3b7090382d997caa2ff4b32baa8c9720e4f57b
Ákos Kappel <kappelakos@gmail.com>
2022-12-13 09:01:28 +0100

    day 13

--- 2022/src/day13.ts
+++ 2022/src/day13.ts
@@ -1,22 +1,37 @@
-import * as day from '../examples/day13.input';
+const parse = (s: string): any[][] => s.trim()
+  .split('\n\n')
+  .map((block: string) => block.split('\n').map(packet => JSON.parse(packet)));
 
-const parse = (s: string) => s.trim();
+const compare = (left: any, right: any): number => {
+  // both are numbers
+  if ([left, right].every(value => +value === value)) return left - right;
 
-export const part1 = (s: string): number => {
-  parse(s);
-  return 0;
+  // transform inner numbers to arrays if necessary
+  [left, right] = [left, right].map(value => (+value === value ? [value] : value));
+
+  // recursively compare nested arrays
+  return left.reduce((acc: number, value: any, i: number) =>
+    acc || compare(value, right[i] ?? value), 0) || left.length - right.length;
 };
+
+export const part1 = (s: string): number => parse(s)
+  .map((g: any[], i: number): [any[], number] => [g, i])
+  .filter(([[l, r]]) => compare(l, r) < 0)
+  .map(([_, i]: [any[], number]) => i + 1)
+  .reduce((acc: number, v: number) => acc + v, 0);
 
 exports.first = part1;
 
 export const part2 = (s: string): number => {
-  parse(s);
-  return 0;
+  const dividerPackets = [[[2]], [[6]]];
+  return parse(s)
+    .flat()
+    .concat(dividerPackets)
+    .sort(compare)
+    .map((v, i) => [v, i])
+    .filter(([v]) => dividerPackets.some(packet => packet === v))
+    .map(([_, i]) => i + 1)
+    .reduce((acc, v) => acc * v, 1);
 };
 
 exports.second = part2;
-
-console.log(part1(day.input));
-console.log(day.answer1);
-console.log(part2(day.input));
-console.log(day.answer2);
--- 2022/tests/day14.test.ts
+++ 2022/tests/day14.test.ts
@@ -1,25 +1,25 @@
 import { existsSync, readFileSync } from 'fs';
-import { part1, part2 } from '../src/day13';
-import * as day from '../examples/day13.input';
+import { part1, part2 } from '../src/day14';
+import * as day from '../examples/day14.input';
 
-const puzzleAnswer1 = 5366;
-const puzzleAnswer2 = 23391;
+const puzzleAnswer1 = 0;
+const puzzleAnswer2 = 0;
 
-const inputFile: string = `input/day13.in`;
+const inputFile: string = `input/day14.in`;
 const puzzleInput: string = existsSync(inputFile) ? readFileSync(inputFile, 'utf8') : '0';
 
-test(`day 13-1 example`, () => {
+test(`day 14-1 example`, () => {
   expect(part1(day.input)).toBe(day.answer1);
 });
 
-test(`day 13-2 example`, () => {
+test(`day 14-2 example`, () => {
   expect(part2(day.input)).toBe(day.answer2);
 });
 
-test(`day 13-1 puzzle`, () => {
+test(`day 14-1 puzzle`, () => {
   expect(part1(puzzleInput)).toBe(puzzleAnswer1);
 });
 
-test(`day 13-2 puzzle`, () => {
+test(`day 14-2 puzzle`, () => {
   expect(part2(puzzleInput)).toBe(puzzleAnswer2);
 });
