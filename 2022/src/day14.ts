668833bd8b20b1f044154df919a34f654c951731
Ákos Kappel <kappelakos@gmail.com>
2022-12-14 11:21:11 +0100

    day 14 part 2

--- 2022/src/day14.ts
+++ 2022/src/day14.ts
@@ -1,5 +1,3 @@
-import * as day from '../examples/day14.input';
-
 const SPAWN_POINT: number[] = [500, 0];
 const X_DIRS: number[] = [-1, 0, 1];
 
@@ -24,32 +22,33 @@ const buildCave = (input: string, addFloor: boolean = false) => {
         [Math.min(minX, x), Math.min(minY, y), Math.max(maxX, x), Math.max(maxY, y)],
       [Infinity, Infinity, -Infinity, -Infinity]);
 
-  const floor: number[][] = [];
+  // create floor for part 2
+  const floor: number[][][] = [];
   if (addFloor) {
-    const height = maxY - minY + 1;
-    const floorY = maxY - minY + 2;
-    const floorX1 = Math.max(spawnX - minX - height, 0);
-    const floorX2 = spawnX - minX + height;
+    const floorY = maxY + 2;
+    const floorStartX = spawnX - floorY - 1;
+    const floorEndX = spawnX + floorY + 1;
     maxY = floorY;
-    minX = floorX1;
-    maxX = floorX2;
-    floor.concat([[floorX1, floorY], [floorX2, floorY]]);
-    console.log(floor);
+    minX = floorStartX;
+    maxX = floorEndX;
+    floor.push([[floorStartX, floorY], [floorEndX, floorY]]);
   }
 
+  // build grid for the cave
   const cave = new Array(maxY - minY + 1).fill(0)
     .map(() => new Array(maxX - minX + 1).fill(EMPTY));
 
+  // fill in the cave with lines of rock
   lines.concat(floor).forEach(line => {
     line.reduce((start: number[], end: number[]) => {
       const [x1, y1] = start;
       const [x2, y2] = end;
 
-      if (x1 === x2) { // draw vertical line
+      if (x1 === x2) { // vertical line
         for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
           cave[y - minY][x1 - minX] = ROCK;
         }
-      } else if (y1 === y2) { // draw horizontal line
+      } else if (y1 === y2) { // horizontal line
         for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
           cave[y1 - minY][x - minX] = ROCK;
         }
@@ -58,13 +57,18 @@ const buildCave = (input: string, addFloor: boolean = false) => {
       return end;
     });
   });
+
+  // display spawn point
   cave[spawnY - minY][spawnX - minX] = HOLE;
+
+  // console.log(cave.map(line => line.join('')).join('\n'));
+  // console.log('width:', cave[0].length, 'height:', cave.length);
 
   return { cave, minX, minY, maxX, maxY };
 };
 
 export const part1 = (s: string): number => {
-  const { cave, minX, minY, maxX, maxY } = buildCave(s);
+  const { cave, minX, minY } = buildCave(s);
 
   let numSandTiles = 0;
   let sand = [SPAWN_POINT[0] - minX, SPAWN_POINT[1] - minY];
@@ -101,18 +105,40 @@ export const part1 = (s: string): number => {
 exports.first = part1;
 
 export const part2 = (s: string): number => {
-  const { cave, minX, minY, maxX, maxY } = buildCave(s, true);
+  const { cave, minX, minY } = buildCave(s, true);
+  const spawn = [SPAWN_POINT[0] - minX, SPAWN_POINT[1] - minY];
 
   let numSandTiles = 0;
+  let sand = spawn;
 
-  console.log(cave.map(line => line.join('')).join('\n'));
+  let keepSpawning: boolean = true;
+  while (keepSpawning) {
+    const [x, y] = sand;
+
+    // can move down
+    if (X_DIRS.some(dx => cave[y + 1][x + dx] === EMPTY)) {
+      if (cave[y + 1][x] === EMPTY) { // move straight down
+        sand = [x, y + 1];
+      } else if (cave[y + 1][x - 1] === EMPTY) { // move down left
+        sand = [x - 1, y + 1];
+      } else if (cave[y + 1][x + 1] === EMPTY) { // move down right
+        sand = [x + 1, y + 1];
+      }
+    }
+    // fell out of bounds
+    else if (sand === spawn || X_DIRS.some(dx => !cave[y + 1][x + dx])) {
+      numSandTiles++;
+      keepSpawning = false;
+    }
+    // can't move any further down, spawn new sand particle
+    else {
+      sand = spawn;
+      cave[y][x] = SAND;
+      numSandTiles++;
+    }
+  }
 
   return numSandTiles;
 };
 
 exports.second = part2;
-
-console.log(part1(day.input));
-console.log(day.answer1);
-// console.log(part2(day.input));
-// console.log(day.answer2);
