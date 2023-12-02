02ca39b6f8699e45ec3d07a4bb2640e7d0ea2e33
Ákos Kappel <kappelakos@gmail.com>
2023-12-02 09:15:03 +0100

    day 2

--- 2023/src/day2.js
+++ 2023/src/day2.js
@@ -1,9 +1,46 @@
-const part1 = (input) => {
-  return 0;
-}
+const parse = (games) => games.trim()
+  .split('\n')
+  .map(game => {
+    const [gameNumber, ...gameHistory] = game.split(':');
+    return {
+      id: parseInt(gameNumber.replace('Game', '').trim()),
+      sets: gameHistory.flatMap(line => line.split(';')
+        .map(round => ({
+            red: /(\d+) red/.test(round) ? parseInt(/(\d+) red/.exec(round)[1]) : 0,
+            blue: /(\d+) blue/.test(round) ? parseInt(/(\d+) blue/.exec(round)[1]) : 0,
+            green: /(\d+) green/.test(round) ? parseInt(/(\d+) green/.exec(round)[1]) : 0,
+          }),
+        )),
+    };
+  });
 
-const part2 = (input) => {
-  return 0;
-}
+const part1 = (input) => {
+  const bag = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+
+  const allGames = parse(input);
+  const validGames = allGames.filter(game =>
+    game.sets.every(set =>
+      set.red <= bag.red &&
+      set.green <= bag.green &&
+      set.blue <= bag.blue,
+    ),
+  );
+
+  return validGames.reduce((acc, game) => acc + game.id, 0);
+};
+
+const part2 = (input) => parse(input)
+  .map(game =>
+    game.sets.reduce((acc, set) => ({
+      red: Math.max(acc.red, set.red),
+      green: Math.max(acc.green, set.green),
+      blue: Math.max(acc.blue, set.blue),
+    }), { red: 0, green: 0, blue: 0 }),
+  )
+  .reduce((acc, game) => acc + game.red * game.green * game.blue, 0);
 
 module.exports = { part1, part2 };
--- 2023/tests/day2.test.js
+++ 2023/tests/day2.test.js
@@ -1,22 +1,28 @@
 const fs = require('fs');
 const { part1, part2 } = require('../src/day2');
 
-const example1 = ``;
+const example1 = `
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`.trim();
 
 const input = fs.readFileSync('inputs/input2.txt', 'utf8');
 
 test('part 1 example', () => {
-  expect(part1(example1)).toBe(0);
+  expect(part1(example1)).toBe(8);
 });
 
 test('part 2 example', () => {
-  expect(part2(example1)).toBe(0);
+  expect(part2(example1)).toBe(2286);
 });
 
 test('part 1 solution', () => {
-  expect(part1(input)).toBe(0);
+  expect(part1(input)).toBe(2377);
 });
 
 test('part 2 solution', () => {
-  expect(part2(input)).toBe(0);
+  expect(part2(input)).toBe(71220);
 });
