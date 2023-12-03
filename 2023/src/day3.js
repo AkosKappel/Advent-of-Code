48424b2d2e331157a15baff06f49f50f1343cedb
Ákos Kappel <kappelakos@gmail.com>
2023-12-03 09:37:29 +0100

    day 3

--- 2023/src/day3.js
+++ 2023/src/day3.js
@@ -1,9 +1,78 @@
+const slice = (str, start, end) => str
+  ?.slice(
+    Math.max(0, start),
+    Math.min(end, str.length),
+  );
+
+const hasNeighborSymbol = str => str
+  ?.replaceAll('.', '')
+  .replaceAll(/\d/g, '')
+  .length;
+
 const part1 = (input) => {
-  return 0;
-}
+  let sum = 0;
+  const lines = input.split('\n');
+
+  lines.forEach((line, row) => {
+    for (let i = 0; i < line.length; i++) {
+      if (line[i].match(/\d/)) {
+        const number = line.slice(i, line.length).match(/^\d+/)[0];
+
+        // get symbols around number
+        const left = line[i - 1];
+        const right = line[i + number.length];
+        const top = slice(lines[row - 1], i - 1, i + number.length + 1);
+        const bottom = slice(lines[row + 1], i - 1, i + number.length + 1);
+
+        // if any array of symbols contains not a dot, add to sum
+        if ([left, right, top, bottom].some(hasNeighborSymbol)) {
+          sum += Number(number);
+        }
+
+        // jump to the end of number
+        i += number.length - 1;
+      }
+    }
+  });
+
+  return sum;
+};
+
+const getNeighborNumbers = (line, i, dist = 1) =>
+  [...line?.matchAll(/\d+/g) || []]
+    .map(match => ({
+      start: match.index,
+      end: match.index + match[0].length - 1,
+      value: match[0],
+    }))
+    // keep only matches whose index (start or end) is within 1 step from symbol
+    .filter(match =>
+      Math.abs(match.start - i) <= dist || Math.abs(match.end - i) <= dist)
+    .map(match => match.value);
 
 const part2 = (input) => {
-  return 0;
-}
+  let sum = 0;
+  const lines = input.split('\n');
+
+  lines.forEach((line, row) => {
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] === '*') {
+        // get numbers around symbol
+        const left = line.slice(0, i).match(/\d+$/)?.[0];
+        const right = line.slice(i + 1, line.length).match(/^\d+/)?.[0];
+        const top = getNeighborNumbers(lines[row - 1], i);
+        const bottom = getNeighborNumbers(lines[row + 1], i);
+
+        // add only if exactly 2 numbers around symbol
+        const nonNull = [left, right, ...top, ...bottom].filter(Boolean);
+        if (nonNull.length === 2) {
+          sum += nonNull.reduce((acc, cur) => acc * Number(cur), 1);
+        }
+      }
+    }
+  });
+
+  return sum;
+};
 
 module.exports = { part1, part2 };
--- 2023/tests/day3.test.js
+++ 2023/tests/day3.test.js
@@ -1,26 +1,37 @@
 const fs = require('fs');
 const { part1, part2 } = require('../src/day3');
 
-const example1 = ``.trim();
+const example1 = `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+`.trim();
 
 const input = fs.readFileSync('inputs/input3.txt', 'utf8');
 
 describe('part 1', () => {
   test('example', () => {
-    expect(part1(example1)).toBe(0);
+    expect(part1(example1)).toBe(4361);
   });
 
   test('solution', () => {
-    expect(part1(input)).toBe(0);
+    expect(part1(input)).toBe(532445);
   });
 });
 
 describe('part 2', () => {
   test('example', () => {
-    expect(part2(example1)).toBe(0);
+    expect(part2(example1)).toBe(467835);
   });
 
   test('solution', () => {
-    expect(part2(input)).toBe(0);
+    expect(part2(input)).toBe(79842967);
   });
 });
