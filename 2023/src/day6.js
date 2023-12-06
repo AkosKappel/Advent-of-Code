d71b7b3a835e57dc8280c0e7305e307521ff7a5d
Ákos Kappel <kappelakos@gmail.com>
2023-12-06 10:39:52 +0100

    refactor day 6

--- 2023/src/day6.js
+++ 2023/src/day6.js
@@ -9,13 +9,21 @@ const parse = (input, withKerning) => {
   return times.map((time, i) => ({ time, distance: distances[i] }));
 };
 
-const solve = (input, withKerning) => parse(input, withKerning)
+const part1 = (input) => parse(input, false)
   .reduce((prod, doc) => prod * Array
     .from({ length: doc.time + 1 }, (_, speed) => speed * (doc.time - speed))
     .filter(distance => distance > doc.distance).length, 1);
 
-const part1 = (input) => solve(input, false);
+const solveQuadratic = (a, b, c) => {
+  const delta = b * b - 4 * a * c;
+  if (delta < 0) return [];
+  if (delta === 0) return [-b / (2 * a)];
+  return [(-b + Math.sqrt(delta)) / (2 * a), (-b - Math.sqrt(delta)) / (2 * a)];
+};
 
-const part2 = (input) => solve(input, true);
+const part2 = (input) => parse(input, true)
+  .map(({ time, distance }) => solveQuadratic(-1, time, -distance))
+  .map(([a, b]) => [Math.max(a, b), Math.min(a, b)])
+  .map(([a, b]) => Math.ceil(a) - Math.floor(b) - 1)[0];
 
 module.exports = { part1, part2 };
