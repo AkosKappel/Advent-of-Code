7a7d78b7eac214b8a14573050428c0afc7625a29
Ákos Kappel <kappelakos@gmail.com>
2023-12-12 11:40:49 +0100

    refactor day 12

--- 2023/src/day12.js
+++ 2023/src/day12.js
@@ -12,7 +12,62 @@ const parse = (input) => input
     };
   });
 
-const countPossibilities = (springs, counts) => {
+const countPossibilities = (springs, counts, counting = 0, memo = {}) => {
+  const key = `${springs}:${counts.join(',')}:${counting}`;
+  if (memo[key] !== undefined) {
+    return memo[key];
+  }
+
+  if (!springs.length) {
+    memo[key] = counts.length === 0 && counting === 0 ? 1 : 0;
+    return memo[key];
+  }
+
+  let total = 0;
+  const options = springs[0] === UNKNOWN ? [OPERATIONAL, DAMAGED] : [springs[0]];
+
+  for (const option of options) {
+    switch (option) {
+      case DAMAGED:
+        // start / continue counting from next block
+        total += countPossibilities(springs.substring(1), counts, counting + 1, memo);
+        break;
+      case OPERATIONAL:
+        if (!counting) {
+          // move to next block without counting
+          total += countPossibilities(springs.substring(1), counts, 0, memo);
+        } else {
+          if (counts.length > 0 && counts[0] === counting) {
+            // end current block counting and move to next block
+            total += countPossibilities(springs.substring(1), counts.slice(1), 0, memo);
+          }
+        }
+        break;
+    }
+  }
+
+  memo[key] = total;
+  return total;
+};
+
+const part1 = (input) => parse(input)
+  .reduce((acc, { springs, counts }) => acc + countPossibilities(springs + OPERATIONAL, counts), 0);
+
+const unfold = (record, times) => {
+  const { springs, counts } = record;
+  return {
+    springs: [...Array(times).fill(springs)].join(UNKNOWN),
+    counts: [...Array(times).fill(counts)].flat(),
+  };
+};
+
+const part2 = (input) => parse(input)
+  .map(record => unfold(record, 5))
+  .reduce((acc, { springs, counts }) => acc + countPossibilities(springs + OPERATIONAL, counts), 0);
+
+module.exports = { part1, part2 };
+
+const countPossibilitiesFast = (springs, counts) => {
   const memo = {};
 
   const dp = (springIndex, countIndex, total = 0) => {
@@ -47,31 +102,3 @@ const countPossibilities = (springs, counts) => {
 
   return dp(0, 0);
 };
-
-const part1 = (input) => parse(input)
-  .reduce((acc, { springs, counts }) => acc + countPossibilities(springs, counts), 0);
-
-const unfold = (record, times) => {
-  const { springs, counts } = record;
-  return {
-    springs: [...Array(times).fill(springs)].join(UNKNOWN),
-    counts: [...Array(times).fill(counts)].flat(),
-  };
-};
-
-const part2 = (input) => parse(input)
-  .map(record => unfold(record, 5))
-  .reduce((acc, { springs, counts }) => acc + countPossibilities(springs, counts), 0);
-
-module.exports = { part1, part2 };
-
-const example1 = `
-???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1
-`.trim();
-console.log(part1(example1));
-console.log(part2(example1));
