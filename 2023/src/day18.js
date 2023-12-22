293d599cf44cd5e69dd13a129304662a39af29dd
Ákos Kappel <kappelakos@gmail.com>
2023-12-22 19:57:47 +0100

    day 18

--- 2023/src/day18.js
+++ 2023/src/day18.js
@@ -1,12 +1,59 @@
-const part1 = (input) => {
-  return 0;
+const DIRECTIONS = {
+  U: [0, 1], D: [0, -1],
+  R: [1, 0], L: [-1, 0],
 };
 
+const parse = (input) => input
+  .split('\n')
+  .map(line => {
+    const [direction, steps, color] = line.split(' ');
+    return { direction, steps: +steps, color: color.slice(1, -1) };
+  });
+
+const getVerticesAndPerimeter = (plan) => {
+  const vertices = [];
+  let perimeter = 0;
+  let x = 0, y = 0;
+
+  for (const { direction, steps } of plan) {
+    const [dx, dy] = DIRECTIONS[direction];
+    x += dx * steps;
+    y += dy * steps;
+    vertices.push({ x, y });
+    perimeter += steps;
+  }
+  vertices.push({ x: 0, y: 0 });
+
+  return { vertices, perimeter };
+};
+
+const shoeLace = (vertices) => {
+  let area = 0;
+  for (let i = 0; i < vertices.length - 1; i++) {
+    const { x: x1, y: y1 } = vertices[i];
+    const { x: x2, y: y2 } = vertices[i + 1];
+    area += (x2 - x1) * y1;
+  }
+  return Math.abs(area);
+};
+
+const part1 = (input) => {
+  const digPlan = parse(input);
+  const { vertices, perimeter } = getVerticesAndPerimeter(digPlan);
+  return shoeLace(vertices) + perimeter / 2 + 1;
+};
+
+const correctPlan = (plan) => plan.map(({ direction, steps, color }) => ({
+  direction: ['R', 'D', 'L', 'U'][color[color.length - 1]],
+  steps: parseInt(color.slice(1, -1), 16),
+  color: direction + steps,
+}));
+
 const part2 = (input) => {
-  return 0;
+  const digPlan = parse(input);
+  const fixedPlan = correctPlan(digPlan);
+  const { vertices, perimeter } = getVerticesAndPerimeter(fixedPlan);
+  return shoeLace(vertices) + perimeter / 2 + 1;
 };
 
 module.exports = { part1, part2 };
-
-// console.log(part1(example1));
-// console.log(part2(example1));
--- 2023/tests/day18.test.js
+++ 2023/tests/day18.test.js
@@ -1,26 +1,41 @@
 const fs = require('fs');
 const { part1, part2 } = require('../src/day18');
 
-const example1 = ``.trim();
+const example1 = `
+R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+`.trim();
 
 const input = fs.readFileSync('inputs/input18.txt', 'utf8');
 
 describe('part 1', () => {
   test('example', () => {
-    expect(part1(example1)).toBe(0);
+    expect(part1(example1)).toBe(62);
   });
 
   test('solution', () => {
-    expect(part1(input)).toBe(0);
+    expect(part1(input)).toBe(50465);
   });
 });
 
 describe('part 2', () => {
   test('example', () => {
-    expect(part2(example1)).toBe(0);
+    expect(part2(example1)).toBe(952408144115);
   });
 
   test('solution', () => {
-    expect(part2(input)).toBe(0);
+    expect(part2(input)).toBe(82712746433310);
   });
 });
