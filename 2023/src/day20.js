601eec6b3da35547f19a261f28966cd6883f98a5
Ákos Kappel <kappelakos@gmail.com>
2023-12-23 14:41:37 +0100

    day 20 part 1

---
+++ 2023/src/day20.js
@@ -0,0 +1,109 @@
+const parse = (input) => {
+  const flipFlops = {};
+  const conjunctions = {};
+  const graph = {};
+
+  input.split('\n').forEach(line => {
+    const [source, destinations] = line.split(' -> ');
+    const moduleType = source[0];
+    const moduleName = source.replace(/[%&]/, '');
+
+    switch (moduleType) {
+      case '%':
+        flipFlops[moduleName] = false;
+        break;
+      case '&':
+        conjunctions[moduleName] = {};
+        break;
+    }
+    graph[moduleName] = destinations.split(', ');
+  });
+
+  for (const [src, destinations] of Object.entries(graph)) {
+    for (const dst of destinations) {
+      if (conjunctions[dst]) {
+        conjunctions[dst][src] = false;
+      }
+    }
+  }
+
+  return { flipFlops, conjunctions, graph };
+};
+
+const pushButton = (graph, flipFlops, conjunctions) => {
+  let nLow = 0, nHigh = 0;
+  const queue = [{
+    sender: 'button',
+    receiver: 'broadcaster',
+    pulse: false,
+  }];
+
+  while (queue.length) {
+    const { sender, receiver, pulse } = queue.shift();
+    // console.log(sender, pulse ? '-high->' : '-low->', receiver);
+    pulse ? nHigh++ : nLow++;
+
+    let nextPulse;
+
+    if (receiver in flipFlops) {
+      if (pulse) continue;
+      flipFlops[receiver] = !flipFlops[receiver];
+      nextPulse = flipFlops[receiver];
+    } else if (receiver in conjunctions) {
+      conjunctions[receiver][sender] = pulse;
+      const allPulses = Object.values(conjunctions[receiver]);
+      nextPulse = !allPulses.every(p => p);
+    } else if (receiver in graph) {
+      nextPulse = pulse;
+    } else {
+      continue;
+    }
+
+    for (const nextReceiver of graph[receiver]) {
+      queue.push({
+        sender: receiver,
+        receiver: nextReceiver,
+        pulse: nextPulse,
+      });
+    }
+  }
+
+  return { nLow, nHigh };
+};
+
+const part1 = (input) => {
+  const { flipFlops, conjunctions, graph } = parse(input);
+
+  let nTotalLow = 0, nTotalHigh = 0;
+  for (let i = 0; i < 1_000; i++) {
+    const { nLow, nHigh } = pushButton(graph, flipFlops, conjunctions);
+    nTotalLow += nLow;
+    nTotalHigh += nHigh;
+  }
+
+  return nTotalLow * nTotalHigh;
+};
+
+const part2 = (input) => {
+  return 0;
+};
+
+module.exports = { part1, part2 };
+
+const example1 = `
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+`.trim();
+const example2 = `
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+`.trim();
+console.log(part1(example1));
+console.log(part1(example2));
+// console.log(part2(example1));
---
+++ 2023/tests/day20.test.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { part1, part2 } = require('../src/day20');
+
+const example1 = `
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+`.trim();
+const example2 = `
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+`.trim();
+
+const input = fs.readFileSync('inputs/input20.txt', 'utf8');
+
+describe('part 1', () => {
+  test('example 1', () => {
+    expect(part1(example1)).toBe(32000000);
+  });
+
+  test('example 2', () => {
+    expect(part1(example2)).toBe(11687500);
+  });
+
+  test('solution', () => {
+    expect(part1(input)).toBe(834323022);
+  });
+});
+
+describe('part 2', () => {
+  test('example', () => {
+    expect(part2(example1)).toBe(0);
+  });
+
+  test('solution', () => {
+    expect(part2(input)).toBe(0);
+  });
+});
