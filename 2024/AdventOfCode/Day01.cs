4663ea554fc13ff7dacc4df114ab6b0d6d1c8679
Akos Kappel <akos.kappel@svarba.sk>
2024-12-01 15:37:33 +0100

    init AoC project 2024

---
+++ 2024/AdventOfCode/Day01.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode;
+
+public class Day01 : BaseDay
+{
+    private readonly string _input;
+    public override string InputFilePath { get; } = "Inputs/01-Example.txt";
+
+    public Day01() : this(null) { }
+    public Day01(string? filename)
+    {
+        _input = File.ReadAllText(filename ?? InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var numbers = _input.Split(',').Select(int.Parse).ToList();
+        var sum = numbers.Sum();
+        return new($"{sum}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var numbers = _input.Split(',').Select(int.Parse).ToList();
+        var sum = numbers.Sum() - 1;
+        return new($"{sum}");
+    }
+}
---
+++ 2024/AdventOfCode/Tests/Day01Test.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode.Tests;
+
+[TestFixture]
+public class Day01Test
+{
+    private Day01 _day;
+
+    [Test]
+    public void TestPart1Example()
+    {
+        _day = new Day01("Inputs/01-Example.txt");
+        var expected = "6";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Input()
+    {
+        _day = new Day01("Inputs/01.txt");
+        var expected = "15";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example()
+    {
+        _day = new Day01("Inputs/01-Example.txt");
+        var expected = "6";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Input()
+    {
+        _day = new Day01("Inputs/01.txt");
+        var expected = "15";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+}
