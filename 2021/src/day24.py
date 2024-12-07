4810afd589f08301df3c67a70cf24dd75d86fee6
Akos Kappel <akos.kappel@svarba.sk>
2024-12-07 21:58:28 +0100

    2021 day 24

--- 2021/src/day24.py
+++ 2021/src/day24.py
@@ -1,34 +1,68 @@
-# TODO: implement
-operations = {
-    'inp': lambda a: a,
-    'add': lambda a, b: a + b,
-    'mul': lambda a, b: a * b,
-    'div': lambda a, b: a // b,
-    'mod': lambda a, b: a % b,
-    'eql': lambda a, b: int(a == b),
-}
+from collections import deque
+from functools import reduce
 
 
 def part1(filename: str):
-    instructions = parse(filename)
-    x = y = z = w = 0
-
-    for ins in instructions:
-        print(f'{operations[ins[0]](ins[1:])}')
-
-    return 0
+    constraints = parse(filename)
+    n_max, _ = find_max_min(constraints)
+    return n_max
 
 
 def part2(filename: str):
-    inputs = parse(filename)
+    constraints = parse(filename)
+    _, n_min = find_max_min(constraints)
+    return n_min
 
-    return 0
 
+def parse(filename: str):
+    constraints = []
+    stack = deque()
 
-def parse(filename: str) -> list[list[str]]:
+    def skip(file, n: int):
+        for _ in range(n):
+            next(file)
+
     with open(filename, mode='r') as f:
-        lines = f.read().split('\n')
-    return [line.split(' ') for line in lines]
+        for i in range(14):
+            skip(f, 4)
+            op = next(f).rstrip()
+            assert op.startswith('div z '), 'Invalid input!'
+
+            if op == 'div z 1':
+                skip(f, 10)
+                op = next(f)
+                assert op.startswith('add y '), 'Invalid input!'
+
+                a = int(op.split()[-1])
+                stack.append((i, a))
+                skip(f, 2)
+            else:
+                op = next(f)
+                assert op.startswith('add x '), 'Invalid input!'
+
+                b = int(op.split()[-1])
+                j, a = stack.pop()
+                constraints.append((i, j, a + b))
+                skip(f, 12)
+
+    return constraints
+
+
+def find_max_min(constraints):
+    n_max = [0] * 14
+    n_min = [0] * 14
+
+    for i, j, diff in constraints:
+        if diff > 0:
+            n_max[i], n_max[j] = 9, 9 - diff
+            n_min[i], n_min[j] = 1 + diff, 1
+        else:
+            n_max[i], n_max[j] = 9 + diff, 9
+            n_min[i], n_min[j] = 1, 1 - diff
+
+    n_max = reduce(lambda acc, d: acc * 10 + d, n_max)
+    n_min = reduce(lambda acc, d: acc * 10 + d, n_min)
+    return n_max, n_min
 
 
 if __name__ == '__main__':
