74acb8b9e3b07742aebf46a3a9cd780078ad361b
Akos Kappel <akos.kappel@svarba.sk>
2024-12-08 11:25:27 +0100

    day 6 - improve performance

--- 2024/AdventOfCode/Day06.cs
+++ 2024/AdventOfCode/Day06.cs
@@ -34,25 +34,20 @@ public class Day06 : BaseDay {
     private bool IsWithinBounds(Vector2 coord) => 0 <= coord.X && coord.X < _width &&
                                                   0 <= coord.Y && coord.Y < _height;
 
-    private List<(Vector2, Vector2)> WalkPatrol(
+    // initial grid should not have loops, so we skip loop detection in this function
+    private List<(Vector2 pos, Vector2 dir)> GetPatrolPath(
         Vector2 currentPosition,
         Vector2 currentDirection,
         HashSet<Vector2> obstructions
     ) {
         var path = new List<(Vector2, Vector2)>();
-        var visited = new HashSet<(Vector2, Vector2)>();
 
         while (IsWithinBounds(currentPosition)) {
-            var current = (currentPosition, currentDirection);
-            if (visited.Contains(current)) return null; // loop detected
-
-            path.Add(current);
-            visited.Add(current);
-
             var nextPosition = currentPosition + currentDirection;
             var isEmpty = !obstructions.Contains(nextPosition);
 
             if (isEmpty) {
+                path.Add((currentPosition, currentDirection));
                 currentPosition = nextPosition;
             }
             else {
@@ -63,23 +58,48 @@ public class Day06 : BaseDay {
         return path;
     }
 
+    private bool IsGuardStuckInLoop(
+        Vector2 currentPosition,
+        Vector2 currentDirection,
+        HashSet<Vector2> obstructions
+    ) {
+        var visited = new HashSet<(Vector2, Vector2)>();
+
+        while (IsWithinBounds(currentPosition)) {
+            // move in current direction as long as possible
+            var nextPosition = currentPosition + currentDirection;
+            while (IsWithinBounds(nextPosition) && !obstructions.Contains(nextPosition)) {
+                nextPosition += currentDirection;
+            }
+
+            if (!IsWithinBounds(nextPosition)) return false;
+
+            currentPosition = nextPosition - currentDirection; // step back to last valid position
+            currentDirection = currentDirection.RotateRight();
+
+            // store and check if we have been here before, to detect loops
+            if (!visited.Add((currentPosition, currentDirection))) return true;
+        }
+
+        return false;
+    }
+
     public override ValueTask<string> Solve_1() {
-        var visitedTiles = WalkPatrol(_startingPosition, _startingDirection, _obstructions)!;
-        var numTiles = visitedTiles.Select(tile => tile.Item1).Distinct().Count();
-        return new(numTiles.ToString());
+        var path = GetPatrolPath(_startingPosition, _startingDirection, _obstructions);
+        var numVisitedTiles = path.Select(tile => tile.pos).Distinct().Count();
+        return new(numVisitedTiles.ToString());
     }
 
     public override ValueTask<string> Solve_2() {
-        var visitedTiles = WalkPatrol(_startingPosition, _startingDirection, _obstructions)!
-            .Where(tile => !_obstructions.Contains(tile.Item1 + tile.Item2)) // skip existing obstructions
-            .DistinctBy(tile => tile.Item1 + tile.Item2); // keep only first occurrence of each tile
+        var startingPoints = GetPatrolPath(_startingPosition, _startingDirection, _obstructions)
+            .DistinctBy(tile => tile.pos + tile.dir); // discard duplicate positions where path crosses itself
 
-        var numNewObstructions = visitedTiles.Count(tile => {
-            var newObstruction = tile.Item1 + tile.Item2;
+        var numNewObstructions = startingPoints.Count(start => {
+            var newObstruction = start.pos + start.dir; // add new obstruction right in front of guard
             _obstructions.Add(newObstruction);
-            var newPath = WalkPatrol(tile.Item1, tile.Item2, _obstructions);
+            var isLooping = IsGuardStuckInLoop(start.pos, start.dir, _obstructions);
             _obstructions.Remove(newObstruction);
-            return newPath is null;
+            return isLooping;
         });
 
         return new(numNewObstructions.ToString());
