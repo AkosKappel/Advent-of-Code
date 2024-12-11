449ce7e84bd451c1aee906f0dde8efc04c01bbae
Akos Kappel <akos.kappel@svarba.sk>
2024-12-11 09:24:59 +0100

    day 11

---
+++ 2024/AdventOfCode/Day11.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode;
+
+public class Day11 : BaseDay {
+    private readonly Dictionary<long, long> _stones;
+
+    public Day11() : this("") { }
+
+    public Day11(string filename) {
+        var inputFile = string.IsNullOrWhiteSpace(filename) ? InputFilePath : filename;
+        _stones = ParseInput(inputFile);
+    }
+
+    private static Dictionary<long, long> ParseInput(string file) => File
+        .ReadAllText(file)
+        .Split(" ")
+        .Select(long.Parse)
+        .GroupBy(number => number)
+        .ToDictionary(group => group.Key, group => (long)group.Count());
+
+    private static Dictionary<long, long> Blink(Dictionary<long, long> stones) {
+        var newStones = new Dictionary<long, long>();
+
+        foreach (var (stone, count) in stones) {
+            if (stone == 0) {
+                newStones.TryAdd(1, 0);
+                newStones[1] += count;
+                continue;
+            }
+
+            var numDigits = stone.ToString().Length;
+            if (numDigits % 2 == 0) {
+                var leftStone = stone / (long)Math.Pow(10, (double)numDigits / 2);
+                var rightStone = stone % (long)Math.Pow(10, (double)numDigits / 2);
+
+                newStones.TryAdd(leftStone, 0);
+                newStones[leftStone] += count;
+
+                newStones.TryAdd(rightStone, 0);
+                newStones[rightStone] += count;
+                continue;
+            }
+
+            var newStone = stone * 2024;
+            newStones.TryAdd(newStone, 0);
+            newStones[newStone] += count;
+        }
+
+        return newStones;
+    }
+
+    private static Dictionary<long, long> Blink(Dictionary<long, long> stones, int times) => Enumerable
+        .Range(0, times)
+        .Aggregate(stones, (s, _) => Blink(s));
+
+    public override ValueTask<string> Solve_1() {
+        const int timesToBlink = 25;
+        var stones = Blink(_stones, timesToBlink);
+        return new(stones.Values.Sum().ToString());
+    }
+
+    public override ValueTask<string> Solve_2() {
+        const int timesToBlink = 75;
+        var stones = Blink(_stones, timesToBlink);
+        return new(stones.Values.Sum().ToString());
+    }
+}
---
+++ 2024/AdventOfCode/Tests/Day11Test.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode.Tests;
+
+[TestFixture]
+public class Day11Test
+{
+    private Day11 _day;
+
+    [Test]
+    public void TestPart1Example()
+    {
+        _day = new Day11("Inputs/11-Example.txt");
+        var expected = "125681";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Example2()
+    {
+        _day = new Day11("Inputs/11-Example2.txt");
+        var expected = "55312";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Input()
+    {
+        _day = new Day11("Inputs/11.txt");
+        var expected = "207683";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example()
+    {
+        _day = new Day11("Inputs/11-Example.txt");
+        var expected = "149161030616311";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example2()
+    {
+        _day = new Day11("Inputs/11-Example2.txt");
+        var expected = "65601038650482";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Input()
+    {
+        _day = new Day11("Inputs/11.txt");
+        var expected = "244782991106220";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+}
