201ffc6ecbabcf66090fa3767dcfe144690be753
Akos Kappel <akos.kappel@svarba.sk>
2024-12-12 09:07:12 +0100

    day 12 part 1

---
+++ 2024/AdventOfCode/Day12.cs
@@ -0,0 +1,91 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode;
+
+public class Day12 : BaseDay {
+    private readonly Dictionary<int, HashSet<Vector2>> _gardenPlots;
+    public override string InputFilePath { get; } = "Inputs/12-Example.txt";
+
+    public Day12() : this("") { }
+
+    public Day12(string filename) {
+        var inputFile = string.IsNullOrWhiteSpace(filename) ? InputFilePath : filename;
+        _gardenPlots = ParseInput(inputFile);
+    }
+
+    private static Dictionary<int, HashSet<Vector2>> ParseInput(string file) {
+        var grid = File.ReadAllText(file).Split("\n").Select(row => row.Trim()).ToList();
+        var width = grid.Max(row => row.Length);
+        var height = grid.Count;
+
+        var garden = new Dictionary<int, HashSet<Vector2>>();
+        var visited = new HashSet<Vector2>();
+
+        var id = 0;
+        for (var y = 0; y < height; y++) {
+            for (var x = 0; x < width; x++) {
+                var current = new Vector2(x, y);
+                if (!visited.Add(current)) {
+                    continue;
+                }
+
+                var region = ExploreRegion(current, grid, width, height, visited);
+                garden.Add(id++, region);
+            }
+        }
+
+        return garden;
+    }
+
+    private static HashSet<Vector2> ExploreRegion(
+        Vector2 start, List<string> grid, int width, int height, HashSet<Vector2> visited
+    ) {
+        var region = new HashSet<Vector2>();
+        var queue = new Queue<Vector2>();
+        queue.Enqueue(start);
+
+        while (queue.Count > 0) {
+            var current = queue.Dequeue();
+            region.Add(current);
+
+            foreach (var dir in Directions.Cardinal) {
+                var neighbor = current + dir;
+                if (0 > neighbor.X || neighbor.X >= width || 0 > neighbor.Y || neighbor.Y >= height) continue;
+                if (visited.Contains(neighbor)) continue;
+                if (grid[(int)current.Y][(int)current.X] != grid[(int)neighbor.Y][(int)neighbor.X]) continue;
+
+                queue.Enqueue(neighbor);
+                visited.Add(neighbor);
+            }
+        }
+
+        return region;
+    }
+
+    private static int GetArea(HashSet<Vector2> region) => region.Count;
+
+    private static int GetPerimeter(HashSet<Vector2> region) =>
+        region.Sum(p => Directions.Cardinal.Count(dir => !region.Contains(p + dir)));
+
+    private static int GetNumberOfSides(HashSet<Vector2> region) {
+        return 0;
+    }
+
+    public override ValueTask<string> Solve_1() => new(_gardenPlots
+        .Select(entry => {
+            var region = entry.Value;
+            return GetArea(region) * GetPerimeter(region);
+        })
+        .Sum()
+        .ToString()
+    );
+
+    public override ValueTask<string> Solve_2() => new(_gardenPlots
+        .Select(entry => {
+            var region = entry.Value;
+            return GetArea(region) * GetNumberOfSides(region);
+        })
+        .Sum()
+        .ToString()
+    );
+}
---
+++ 2024/AdventOfCode/Tests/Day12Test.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode.Tests;
+
+[TestFixture]
+public class Day12Test {
+    private Day12 _day;
+
+    [Test]
+    public void TestPart1Example() {
+        _day = new Day12("Inputs/12-Example.txt");
+        var expected = "140";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Example2() {
+        _day = new Day12("Inputs/12-Example2.txt");
+        var expected = "772";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Example3() {
+        _day = new Day12("Inputs/12-Example3.txt");
+        var expected = "1930";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Input() {
+        _day = new Day12("Inputs/12.txt");
+        var expected = "1449902";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example() {
+        _day = new Day12("Inputs/12-Example.txt");
+        var expected = "80";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example2() {
+        _day = new Day12("Inputs/12-Example2.txt");
+        var expected = "436";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example3() {
+        _day = new Day12("Inputs/12-Example3.txt");
+        var expected = "1206";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example4() {
+        _day = new Day12("Inputs/12-Example4.txt");
+        var expected = "236";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example5() {
+        _day = new Day12("Inputs/12-Example5.txt");
+        var expected = "368";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Input() {
+        _day = new Day12("Inputs/12.txt");
+        var expected = "0";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+}
