6a0b0965b40b40243ac98f6bff2a1ed558b016d8
Akos Kappel <akos.kappel@svarba.sk>
2024-12-17 09:23:07 +0100

    day 17 part 1

---
+++ 2024/AdventOfCode/Day17.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day17 : BaseDay {
+    private readonly (int A, int B, int C) _register;
+    private readonly List<int> _program;
+    // public override string InputFilePath { get; } = "Inputs/17-Example.txt";
+
+    public Day17() : this("") { }
+
+    public Day17(string filename) {
+        var inputFile = string.IsNullOrWhiteSpace(filename) ? InputFilePath : filename;
+        (_register, _program) = ParseInput(inputFile);
+    }
+
+    private static ((int A, int B, int C), List<int>) ParseInput(string file) {
+        var parts = File.ReadAllText(file).ReplaceLineEndings("\n").Split("\n\n");
+        var regex = new Regex(@"Register A: (?<a>-?\d+)\nRegister B: (?<b>-?\d+)\nRegister C: (?<c>-?\d+)",
+            RegexOptions.Compiled);
+        var program = parts[1].Replace("Program: ", "").Split(",").Select(int.Parse).ToList();
+        var match = regex.Match(parts[0]);
+        var register = (
+            int.Parse(match.Groups["a"].Value),
+            int.Parse(match.Groups["b"].Value),
+            int.Parse(match.Groups["c"].Value)
+        );
+        return (register, program);
+    }
+
+    private static List<int> Run((int A, int B, int C) register, List<int> program) {
+        var output = new List<int>();
+
+        for (var pointer = 0; pointer < program.Count; pointer += 2) {
+            var opcode = program[pointer];
+            var operand = program[pointer + 1];
+
+            switch (opcode) {
+                case 0:
+                    register.A /= (int)Math.Pow(2, Combo(operand));
+                    break;
+                case 1:
+                    register.B ^= operand;
+                    break;
+                case 2:
+                    register.B = Combo(operand) % 8;
+                    break;
+                case 3:
+                    if (register.A != 0) pointer = operand - 2;
+                    break;
+                case 4:
+                    register.B ^= register.C;
+                    break;
+                case 5:
+                    output.Add(Combo(operand) % 8);
+                    break;
+                case 6:
+                    register.B = register.A / (int)Math.Pow(2, Combo(operand));
+                    break;
+                case 7:
+                    register.C = register.A / (int)Math.Pow(2, Combo(operand));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Unknown opcode: {opcode}");
+            }
+        }
+
+        return output;
+
+        int Combo(int x) => x switch {
+            >= 0 and <= 3 => x,
+            4 => register.A,
+            5 => register.B,
+            6 => register.C,
+            _ => throw new ArgumentOutOfRangeException(nameof(x), x, null)
+        };
+    }
+
+    public override ValueTask<string> Solve_1() => new(string.Join(",", Run(_register, _program)));
+
+    public override ValueTask<string> Solve_2() {
+        return new(0.ToString());
+    }
+}
---
+++ 2024/AdventOfCode/Tests/Day17Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode.Tests;
+
+[TestFixture]
+public class Day17Test {
+    private Day17 _day;
+
+    [Test]
+    public void TestPart1Example() {
+        _day = new Day17("Inputs/17-Example.txt");
+        const string expected = "4,6,3,5,6,3,5,2,1,0";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Input() {
+        _day = new Day17("Inputs/17.txt");
+        const string expected = "2,1,0,1,7,2,5,0,3";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example() {
+        _day = new Day17("Inputs/17-Example.txt");
+        const string expected = "000";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Input() {
+        _day = new Day17("Inputs/17.txt");
+        const string expected = "000";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+}
