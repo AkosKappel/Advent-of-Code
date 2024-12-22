6c0df5ea1766ca57337ae1633a52fbf986b30799
Akos Kappel <akos.kappel@svarba.sk>
2024-12-22 09:20:04 +0100

    day 22 part 1

---
+++ 2024/AdventOfCode/Day22.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode;
+
+public class Day22 : BaseDay {
+    private readonly long[] _initialSecrets;
+    // public override string InputFilePath { get; } = "Inputs/22-Example.txt";
+
+    public Day22() : this("") { }
+
+    public Day22(string filename) {
+        var inputFile = string.IsNullOrWhiteSpace(filename) ? InputFilePath : filename;
+        _initialSecrets = ParseInput(inputFile);
+    }
+
+    private static long[] ParseInput(string file) => File
+        .ReadAllText(file).ReplaceLineEndings("\n").Split("\n").Select(long.Parse).ToArray();
+
+    private static long EvolveSecret(long secret, int iterations) {
+        for (var i = 0; i < iterations; i++) {
+            secret = Prune(Mix(secret, secret * 64));
+            secret = Prune(Mix(secret, secret / 32));
+            secret = Prune(Mix(secret, secret * 2048));
+        }
+
+        return secret;
+
+        long Mix(long a, long b) => a ^ b;
+        long Prune(long a) => a % 16777216;
+    }
+
+    public override ValueTask<string> Solve_1() => new(
+        _initialSecrets.Sum(s => EvolveSecret(s, 2000)).ToString()
+    );
+
+    public override ValueTask<string> Solve_2() {
+        return new(0.ToString());
+    }
+}
---
+++ 2024/AdventOfCode/Tests/Day22Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode.Tests;
+
+[TestFixture]
+public class Day22Test {
+    private Day22 _day;
+
+    [Test]
+    public void TestPart1Example() {
+        _day = new Day22("Inputs/22-Example.txt");
+        const string expected = "37327623";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart1Input() {
+        _day = new Day22("Inputs/22.txt");
+        const string expected = "13004408787";
+        var solution = _day.Solve_1();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Example() {
+        _day = new Day22("Inputs/22-Example.txt");
+        const string expected = "";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestPart2Input() {
+        _day = new Day22("Inputs/22.txt");
+        const string expected = "";
+        var solution = _day.Solve_2();
+        Assert.That(solution.Result, Is.EqualTo(expected));
+    }
+}
