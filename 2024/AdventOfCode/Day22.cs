00a664e8d1c7105b64360807adebd9ddde454994
Akos Kappel <akos.kappel@svarba.sk>
2024-12-22 15:22:47 +0100

    day 22 part 2 optimization

--- 2024/AdventOfCode/Day22.cs
+++ 2024/AdventOfCode/Day22.cs
@@ -13,10 +13,6 @@ public class Day22 : BaseDay {
     private static long[] ParseInput(string file) => File
         .ReadAllText(file).ReplaceLineEndings("\n").Split("\n").Select(long.Parse).ToArray();
 
-    private static long Mix(long a, long b) => a ^ b;
-    private static long Prune(long a) => a % 16777216;
-    private static int Price(long a) => (int)(a % 10);
-
     private static IEnumerable<long> NextSecret(long secret, int iterations = 2024) {
         for (var i = 0; i < iterations; i++) {
             yield return secret;
@@ -24,6 +20,11 @@ public class Day22 : BaseDay {
             secret = Prune(Mix(secret, secret / 32));
             secret = Prune(Mix(secret, secret * 2048));
         }
+
+        yield break;
+
+        long Mix(long a, long b) => a ^ b;
+        long Prune(long a) => a % 16777216;
     }
 
     public override ValueTask<string> Solve_1() => new(
@@ -32,12 +33,12 @@ public class Day22 : BaseDay {
 
     public override ValueTask<string> Solve_2() {
         const int nLastChanges = 4;
-        var results = new Dictionary<string, int>();
+        var results = new Dictionary<int, int>();
         var recentChanges = new Queue<int>(nLastChanges);
 
         foreach (var initialSecret in _initialSecrets) {
             var previousPrice = Price(initialSecret);
-            var dict = new Dictionary<string, int>();
+            var buyer = new Dictionary<int, int>();
             recentChanges.Clear();
 
             foreach (var secret in NextSecret(initialSecret).Skip(1).Take(2000)) {
@@ -49,14 +50,17 @@ public class Day22 : BaseDay {
                 recentChanges.Enqueue(change);
                 if (recentChanges.Count < nLastChanges) continue;
 
-                dict.TryAdd(string.Join(",", recentChanges), price);
+                buyer.TryAdd(Hash(recentChanges), price);
             }
 
-            foreach (var (id, price) in dict) {
-                results[id] = results.GetValueOrDefault(id, 0) + price;
+            foreach (var (key, price) in buyer) {
+                results[key] = results.GetValueOrDefault(key, 0) + price;
             }
         }
 
         return new(results.Values.Max().ToString());
+
+        int Price(long a) => (int)(a % 10);
+        int Hash(IEnumerable<int> numbers) => numbers.Aggregate(0, (a, b) => (a << 8) + b);
     }
 }
