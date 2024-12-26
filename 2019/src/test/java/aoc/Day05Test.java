5c6b6e9bdcdc9fccdb44fe0a6c1d87bc867b37bc
Akos Kappel <akos.kappel@svarba.sk>
2024-12-26 11:52:38 +0100

    add tests for day 5

--- 2019/src/test/java/aoc/Day05Test.java
+++ 2019/src/test/java/aoc/Day05Test.java
@@ -10,11 +10,17 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day05Test {
+    private static String example;
+    private static String example2;
+    private static String example3;
     private static String input;
     private Day05 day;
 
     @BeforeAll
     public static void init() throws IOException {
+        example = Files.readString(Paths.get("src/main/resources/day05_example.txt"), StandardCharsets.UTF_8);
+        example2 = Files.readString(Paths.get("src/main/resources/day05_example2.txt"), StandardCharsets.UTF_8);
+        example3 = Files.readString(Paths.get("src/main/resources/day05_example3.txt"), StandardCharsets.UTF_8);
         input = Files.readString(Paths.get("src/main/resources/day05.txt"), StandardCharsets.UTF_8);
     }
 
@@ -36,6 +42,24 @@ class Day05Test {
     @Nested
     @DisplayName("Part 2")
     class Part2Tests {
+        @Test
+        void testExample() {
+            long result = day.part2(example);
+            assertEquals(1L, result);
+        }
+
+        @Test
+        void testExample2() {
+            long result = day.part2(example2);
+            assertEquals(1L, result);
+        }
+
+        @Test
+        void testExample3() {
+            long result = day.part2(example3);
+            assertEquals(999L, result);
+        }
+
         @Test
         void testSolution() {
             long result = day.part2(input);
