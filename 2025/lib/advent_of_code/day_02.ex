2a9472d18833a35b0981a08cdeb292bda211343c
Akos Kappel <akos.kappel@igtconsulting.eu>
2025-12-02 20:42:05 +0100

    2025 day 2

--- 2025/lib/advent_of_code/day_02.ex
+++ 2025/lib/advent_of_code/day_02.ex
@@ -1,7 +1,67 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(fn pair ->
+      [from, to] = String.split(pair, "-")
+      {String.to_integer(from), String.to_integer(to)}
+    end)
   end
 
-  def part2(_args) do
+  defp is_repeating_twice(id) do
+    digits = Integer.digits(id)
+    len = length(digits)
+
+    if rem(len, 2) != 0 do
+      # Odd number of digits can't be repeating
+      false
+    else
+      half = div(len, 2)
+      {first, second} = Enum.split(digits, half)
+      first == second
+    end
+  end
+
+  def part1(args) do
+    args
+    |> parse()
+    |> Enum.reduce(0, fn {from, to}, count ->
+      from..to
+      |> Enum.filter(&is_repeating_twice/1)
+      |> Enum.reduce(count, fn id, acc -> acc + id end)
+    end)
+  end
+
+  defp is_repeating_any_times(id) do
+    digits = Integer.digits(id)
+    len = length(digits)
+
+    # A pattern must repeat at least twice, so we need at least 2 digits
+    # and the pattern length must be at most len/2
+    if len < 2 do
+      false
+    else
+      Enum.any?(1..div(len, 2), fn pattern_len ->
+        if rem(len, pattern_len) == 0 do
+          chunks = Enum.chunk_every(digits, pattern_len)
+          first_chunk = hd(chunks)
+          # Check if we have at least 2 chunks and all are the same
+          length(chunks) >= 2 and Enum.all?(chunks, &(&1 == first_chunk))
+        else
+          false
+        end
+      end)
+    end
+  end
+
+  def part2(args) do
+    args
+    |> parse()
+    |> Enum.reduce(0, fn {from, to}, count ->
+      from..to
+      |> Enum.filter(&is_repeating_any_times/1)
+      |> Enum.reduce(count, fn id, acc -> acc + id end)
+    end)
   end
 end
--- 2025/test/advent_of_code/day_02_test.exs
+++ 2025/test/advent_of_code/day_02_test.exs
@@ -3,19 +3,38 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
+  @day 2
+  @year 2025
+
+  @example1 "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+  @tag :skip
+  test "part1 example1" do
+    result = part1(@example1)
+
+    assert result == 1227775554
+  end
+
   @tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(@day, @year)
     result = part1(input)
 
-    assert result
+    assert result == 28844599675
+  end
+
+  @tag :skip
+  test "part2 example1" do
+    result = part2(@example1)
+
+    assert result == 4174379265
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(@day, @year)
     result = part2(input)
 
-    assert result
+    assert result == 48778605167
   end
 end
