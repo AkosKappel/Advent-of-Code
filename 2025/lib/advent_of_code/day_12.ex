b3d2c4c17db5d3c918874db9dd557e118198b531
Akos Kappel <akos.kappel@igtconsulting.eu>
2025-12-13 14:51:25 +0100

    2025 day 12

--- 2025/lib/advent_of_code/day_12.ex
+++ 2025/lib/advent_of_code/day_12.ex
@@ -1,7 +1,26 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_input) do
+  defp parse(input) do
+    Regex.split(~r/\n\s*\n/, input, trim: true)
+    |> Enum.at(-1)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn line ->
+      [area, shapes] = String.split(line, ": ")
+
+      shapes = shapes |> String.split(" ") |> Enum.map(&String.to_integer/1)
+      [w, h] = area |> String.split("x") |> Enum.map(&String.to_integer/1)
+
+      {w, h, shapes}
+    end)
   end
 
-  def part2(_input) do
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.filter(fn {width, height, shapes} ->
+      area = Enum.reduce(shapes, 0, fn v, acc -> acc + 9 * v end)
+      width * height >= area
+    end)
+    |> Enum.count()
   end
 end
--- 2025/test/advent_of_code/day_12_test.exs
+++ 2025/test/advent_of_code/day_12_test.exs
@@ -6,31 +6,52 @@ defmodule AdventOfCode.Day12Test do
   @day 12
   @year 2025
 
-  @example ""
+  @example String.trim("""
+           0:
+           ###
+           ##.
+           ##.
+
+           1:
+           ###
+           ##.
+           .##
+
+           2:
+           .##
+           ###
+           ##.
+
+           3:
+           ##.
+           ###
+           ##.
+
+           4:
+           ###
+           #..
+           ###
+
+           5:
+           ###
+           .#.
+           ###
+
+           4x4: 0 0 0 0 2 0
+           12x5: 1 0 1 0 2 2
+           12x5: 1 0 1 0 3 2
+           """)
 
   test "part1 example" do
     result = part1(@example)
 
-    assert result == nil
+    assert result == 1
   end
 
   test "part1" do
     input = AdventOfCode.Input.get!(@day, @year)
     result = part1(input)
 
-    assert result == nil
-  end
-
-  test "part2 example" do
-    result = part2(@example)
-
-    assert result == nil
-  end
-
-  test "part2" do
-    input = AdventOfCode.Input.get!(@day, @year)
-    result = part2(input)
-
-    assert result == nil
+    assert result == 460
   end
 end
