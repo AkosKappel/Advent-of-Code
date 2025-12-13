26e8238fe2436fa40e826a5367462c0e6ee67539
Akos Kappel <akos.kappel@igtconsulting.eu>
2025-12-13 13:47:30 +0100

    2025 day 11 part 1

--- 2025/lib/advent_of_code/day_11.ex
+++ 2025/lib/advent_of_code/day_11.ex
@@ -1,5 +1,48 @@
 defmodule AdventOfCode.Day11 do
-  def part1(_input) do
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn line ->
+      [source, destionations] = String.split(line, ": ")
+      {source, String.split(destionations, " ")}
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp count_paths(_graph, nil, _target, _visited), do: 0
+  defp count_paths(_graph, start, target, _visited) when start == target, do: 1
+
+  defp count_paths(graph, start, target, visited) do
+    neighbors = Map.get(graph, start, [])
+
+    {total, _final_visited} =
+      Enum.reduce(neighbors, {0, visited}, fn next_label, {sum, visit_map} ->
+        cond do
+          # Direct connection to target
+          next_label == target ->
+            {sum + 1, visit_map}
+
+          # Already computed for this node
+          Map.has_key?(visit_map, next_label) ->
+            {sum + Map.get(visit_map, next_label), visit_map}
+
+          # Recursively compute
+          true ->
+            value = count_paths(graph, next_label, target, visit_map)
+            new_visit_map = Map.put(visit_map, next_label, value)
+            {sum + value, new_visit_map}
+        end
+      end)
+
+    total
+  end
+
+  def part1(input) do
+    input
+    |> parse()
+    |> count_paths("you", "out", %{})
   end
 
   def part2(_input) do
--- 2025/test/advent_of_code/day_11_test.exs
+++ 2025/test/advent_of_code/day_11_test.exs
@@ -6,19 +6,30 @@ defmodule AdventOfCode.Day11Test do
   @day 11
   @year 2025
 
-  @example ""
+  @example String.trim("""
+           aaa: you hhh
+           you: bbb ccc
+           bbb: ddd eee
+           ccc: ddd eee fff
+           ddd: ggg
+           eee: out
+           fff: out
+           ggg: out
+           hhh: ccc fff iii
+           iii: out
+           """)
 
   test "part1 example" do
     result = part1(@example)
 
-    assert result == nil
+    assert result == 5
   end
 
   test "part1" do
     input = AdventOfCode.Input.get!(@day, @year)
     result = part1(input)
 
-    assert result == nil
+    assert result == 674
   end
 
   test "part2 example" do
