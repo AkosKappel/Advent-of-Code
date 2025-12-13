37c969972dc83ef16a159b9e966870033ab02696
Akos Kappel <akos.kappel@igtconsulting.eu>
2025-12-13 12:57:39 +0100

    2025 day 10 part 1

--- 2025/lib/advent_of_code/day_10.ex
+++ 2025/lib/advent_of_code/day_10.ex
@@ -1,7 +1,108 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_input) do
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      # Capture: [lights], (buttons), and {joltages}
+      ~r/\[([.#]+)\]\s+(.*?)\s+\{([\d,]+)\}/
+      |> Regex.run(line)
+      |> case do
+        [_line, lights, wirings, joltages] ->
+          # Convert lights to bitmask
+          lights_bitmask =
+            lights
+            |> String.graphemes()
+            |> Enum.with_index()
+            |> Enum.map(fn {char, idx} ->
+              if char == "#", do: trunc(:math.pow(2, idx)), else: 0
+            end)
+            |> Enum.sum()
+
+          # Parse button schemas
+          wirings =
+            wirings
+            |> String.split(~r/\)\s*\(/, trim: true)
+            |> Enum.map(fn wiring ->
+              wiring
+              |> String.replace(~r/[()]/, "")
+              |> String.split(",")
+              |> Enum.map(&String.to_integer/1)
+            end)
+
+          # Parse joltages
+          joltages =
+            joltages
+            |> String.split(",")
+            |> Enum.map(&String.to_integer/1)
+
+          {lights_bitmask, wirings, joltages}
+      end
+    end)
+  end
+
+  defp index_to_bitmask(indices) do
+    indices
+    |> Enum.map(&trunc(:math.pow(2, &1)))
+    |> Enum.sum()
+  end
+
+  defp bfs(target_lights, schema_bitmasks) do
+    # Start with state 0 (all lights off)
+    initial_state = 0
+    queue = :queue.from_list([{initial_state, 0}])
+    visited = MapSet.new([initial_state])
+
+    bfs(queue, visited, target_lights, schema_bitmasks)
+  end
+
+  defp bfs(queue, visited, target_lights, schema_bitmasks) do
+    case :queue.out(queue) do
+      {:empty, _} ->
+        # Should not happen with valid input
+        :infinity
+
+      {{:value, {current_state, steps}}, new_queue} ->
+        if current_state == target_lights do
+          steps
+        else
+          # Try pressing each button
+          {new_queue, new_visited} =
+            Enum.reduce(schema_bitmasks, {new_queue, visited}, fn schema, {q, v} ->
+              # XOR to toggle the lights affected by this button
+              next_state = Bitwise.bxor(current_state, schema)
+
+              if MapSet.member?(v, next_state) do
+                {q, v}
+              else
+                {
+                  :queue.in({next_state, steps + 1}, q),
+                  MapSet.put(v, next_state)
+                }
+              end
+            end)
+
+          bfs(new_queue, new_visited, target_lights, schema_bitmasks)
+        end
+    end
+  end
+
+  defp solve_machine({target_lights, schemas, _joltages}) do
+    # Convert schemas to bitmasks
+    schema_bitmasks = Enum.map(schemas, &index_to_bitmask/1)
+
+    # BFS to find minimum steps
+    bfs(target_lights, schema_bitmasks)
+  end
+
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.map(&solve_machine/1)
+    |> Enum.sum()
   end
 
   def part2(_input) do
+    nil
   end
 end
--- 2025/test/advent_of_code/day_10_test.exs
+++ 2025/test/advent_of_code/day_10_test.exs
@@ -6,19 +6,23 @@ defmodule AdventOfCode.Day10Test do
   @day 10
   @year 2025
 
-  @example ""
+  @example String.trim("""
+           [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+           [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+           [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+           """)
 
   test "part1 example" do
     result = part1(@example)
 
-    assert result == nil
+    assert result == 7
   end
 
   test "part1" do
     input = AdventOfCode.Input.get!(@day, @year)
     result = part1(input)
 
-    assert result == nil
+    assert result == 486
   end
 
   test "part2 example" do
